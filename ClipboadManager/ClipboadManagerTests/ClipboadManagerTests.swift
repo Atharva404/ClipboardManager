79cfd8e58eca8f2e48bee9b736c0500b2b419719
Atharva Gupta <55639424+Atharva404@users.noreply.github.com>
2025-02-27 00:49:53 -0500

    Add files via upload

---
+++ ClipboadManager/ClipboadManagerTests/ClipboadManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboadManagerTests.swift
+//  ClipboadManagerTests
+//
+//  Created by Atharva Gupta on 2/20/25.
+//
+
+import Testing
+@testable import ClipboadManager
+
+struct ClipboadManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
